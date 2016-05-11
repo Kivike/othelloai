43b89d7f6c7f9f59b03de57dce320cd2e69a0a1a
Roope Rajala <r_rajala@yahoo.com>
2016-05-11 15:40:16 +0300

    Trying with stability/mobility

--- PeetuRoope.py
+++ PeetuRoope.py
@@ -28,26 +28,39 @@ class PeetuRoope(ReversiAlgorithm):
 
 	currentTurn = 0
 
-	START_DEPTH = 0
+	START_DEPTH = 1
 	# How deep will algorithm go? Time is usually more limiting
-	MAX_DEPTH = 12
+	MAX_DEPTH = 10
 
 	currentIterationDepth = 0
 
 	# Timer to see how long the game takes
 	gameStartTime = -1
 
+	# 8x8 array
+	# True = stable
+	stableDiscs = []
+
     # http://dhconnelly.github.io/paip-python/docs/paip/othello.html
 	SCORE_WEIGHTS = [
 	    [ 120, 	-20,  20,  10,  10,   20, -20,  120],
-	    [-20, 	-40,  -5,  -5,  -5,   -5, -40,  -20],
-	    [ 20,  	 -5,  15,   3,   3,   15,  -5,   20],
+	    [-20, 	-500,  -5,  -5,  -5,   -5, -500,  -120],
+	    [ 20,  	 -5,  15,   3,   3,   15,  -5,   50],
 	    [ 10,  	 -5,   3,   3,   3,    3,  -5,   10],
 	    [ 10,  	 -5,   3,   3,   3,    3,  -5,   10],
 	    [ 20,  	 -5,  15,   3,   3,   15,  -5,   20],
-	    [-20, 	-40,  -5,  -5,  -5,   -5, -40,  -20],
-	    [ 120, 	-20,  25,  10,  10,   20, -20,   120]
+	    [-20, 	-500,  -5,  -5,  -5,   -5, -500,  -120],
+	    [ 120, 	-20,  20,  10,  10,   20, -20,   120]
 	]
+
+	STABILITY_WEIGHT = {
+		"stable" 	: 120,
+		"danger" 	: -120,
+		"neutral" 	: 10,
+		"edge"		: 15
+	}
+
+	MOBILITY_WEIGHT = 30
 
 	def __init__(self):
 		threading.Thread.__init__(self);
@@ -67,6 +80,9 @@ class PeetuRoope(ReversiAlgorithm):
 
 		if PeetuRoope.gameStartTime == -1:
 			PeetuRoope.gameStartTime  = time.time()
+
+			for x in range(0, 8):
+				PeetuRoope.stableDiscs.append([False for x in range(8)])
 
 	@property
 	def name(self):
@@ -107,7 +123,7 @@ class PeetuRoope(ReversiAlgorithm):
 
 			rootNode = Node(self.initialState, None)
 
-			self.alphaBetaFromRoot(rootNode, 0, self.currentIterationDepth, self.playerIndex, True)
+			self.alphaBetaFromRoot(rootNode, 0, self.currentIterationDepth, -sys.maxint, sys.maxint, self.playerIndex, True)
 
 			optimalChild = rootNode.getOptimalChild()
 
@@ -147,13 +163,14 @@ class PeetuRoope(ReversiAlgorithm):
 
 		self.controller.doMove(self.bestMove)
 
+		#self.printPossibleMoves(self.initialState)
 		print "Made move " + self.bestMove.toString()
 		self.bestMove = None
 
 
 	# Creates tree starting from root to given depth limit and then
 	# recursively use minmax to score the nodes
-	def alphaBetaFromRoot(self, node, currentIterationDepth, depthLimit, playerIndex, maximize):
+	def alphaBetaFromRoot(self, node, currentIterationDepth, depthLimit, alpha, beta, playerIndex, maximize):
 		if currentIterationDepth > depthLimit:
 			return self.evaluateNodeScore(node)
 
@@ -163,7 +180,7 @@ class PeetuRoope(ReversiAlgorithm):
 			if len(moves) == 0:
 				return self.evaluateNodeScore(node)
 			else:
-				random.shuffle(moves)
+				#random.shuffle(moves)
 
 				for move in moves:
 					newstate = node.state.getNewInstance(move.x, move.y, move.player)
@@ -172,47 +189,155 @@ class PeetuRoope(ReversiAlgorithm):
 		currentIterationDepth += 1
 
 		if maximize:
-			node.score = -10000
+			node.score = -sys.maxint
 
 			for child in node.children:
-				child.score = self.alphaBetaFromRoot(child, currentIterationDepth, depthLimit, 1 - playerIndex, not maximize)
+				child.score = self.alphaBetaFromRoot(child, currentIterationDepth, depthLimit, 
+					alpha, beta, 1 - playerIndex, not maximize)
 
 				if child.score > node.score:
 					node.score = child.score
 
-					if node.parent != None and node.score <= node.parent.score:
-						# Cut off, we're not choosing this one anyway
-						break
+				if child.score > alpha:
+					alpha = child.score
+
+				if beta <= alpha:
+					break
 			return node.score
 		else:
-			node.score = 10000
+			node.score = sys.maxint
 
 			for child in node.children:
-				child.score = self.alphaBetaFromRoot(child, currentIterationDepth, depthLimit, 1 - playerIndex, not maximize)
+				child.score = self.alphaBetaFromRoot(child, currentIterationDepth, depthLimit,
+					alpha, beta, 1 - playerIndex, not maximize)
 
 				if child.score < node.score:
 					node.score = child.score
 
-					if node.parent != None and node.score >= node.parent.score:
-						# Cut off, we're not choosing this one anyway
-						break
+				if child.score < beta:
+					beta = child.score
+
+				if beta <= alpha:
+					break
 			return node.score
 
 	def evaluateNodeScore(self, node):
+		if(self.currentTurn < 5):
+			return self.greedyEvaluate(node)
+		elif(self.currentTurn < 25):
+			return self.weightedEvaluate(node)
+		else:
+			return self.greedyEvaluate(node)
+
+
+	def weightedEvaluate(self, node):
 		score = 0
 		state = node.state
 		move = node.getMove()
 
+		#return state.getMarkCount(move.player)
+
 		for x in range(0, 8):
 			for y in range(0, 8):
-				weight = PeetuRoope.SCORE_WEIGHTS[x][y]
+				mark = state.getMarkAt(x, y)
+				# Check for empty square
+				if mark == -1:
+					continue
 
-				if self.playerIndex != state.getMarkAt(x, y):
-					score -= weight
+				edge = x == 0 or x == 7 or y == 0 or y == 7
+
+				weight = 0
+
+				stability = self.isPositionStable(node, x, y, edge, mark)
+
+				if stability == 1:
+					# Own stable
+					weight = PeetuRoope.STABILITY_WEIGHT["stable"]
+				elif stability == -1:
+					# Opponent stable
+					weight = -PeetuRoope.STABILITY_WEIGHT["danger"]
 				else:
+					# Flippable, seemingly not dangerous
+					if edge:
+						weight = PeetuRoope.STABILITY_WEIGHT["edge"]
+					else:
+						weight = PeetuRoope.STABILITY_WEIGHT["neutral"]
+
+				if self.playerIndex == mark:
 					score += weight
+				else:
+					score -= weight
+
 
 		# Give score based on possible moves for the node
-		score += len(node.children)
+		#score -= node.state.getPossibleMoveCount(1 - move.player) * PeetuRoope.MOBILITY_WEIGHT
 
 		return score
+
+	def greedyEvaluate(self, node):
+		return node.state.getMarkCount(self.playerIndex)
+
+	# Returns 1 if stable
+	# Returns 0 if not stable / neutral / flippable
+	# Returns -1 if dangerous
+	def isPositionStable(self, node, x, y, edge, player):
+		if PeetuRoope.stableDiscs[x][y] == True:
+			return 1
+
+		# Only check stability for edges and corners
+		if not edge:
+			return 0
+
+		dangerInOneDirection = False
+
+		if x == 0 or x == 7:
+			for y in range(y, 8):
+				mark = node.state.getMarkAt(x, y)
+				if mark != player:
+					if mark != -1:
+						dangerInOneDirection = True
+					break
+
+				if y == 7:
+					PeetuRoope.stableDiscs[x][y] = player
+					return 1
+
+			for y in range(y, -1, -1):
+				mark = node.state.getMarkAt(x, y)
+				if mark != player:
+					if mark != -1:
+						dangerInOneDirection = True
+					break
+
+				if y == 0:
+					PeetuRoope.stableDiscs[x][y] = True
+					return 1
+		else:
+			for x in range(x, 8):
+				mark = node.state.getMarkAt(x, y)
+				if mark != player:
+					if mark != -1:
+						dangerInOneDirection = True
+					break
+
+				if x == 7:
+					PeetuRoope.stableDiscs[x][y] = True
+					return 1
+
+			for x in range(x, -1, -1):
+				mark = node.state.getMarkAt(x, y)
+				if mark != player:
+					if mark != -1:
+						dangerInOneDirection = True
+					break
+
+				if x == 0:
+					PeetuRoope.stableDiscs[x][y] = True
+					return 1
+
+		if dangerInOneDirection:
+			# Opponent could flip this next turn
+			return -1
+		else:
+			# Flippable but not dangerous
+			return 0
